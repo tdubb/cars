a716fe3ef6ab55e2e66aed8317af1e498f1ab57f
MakerSquare Student <students@themakersquare.com>
2013-11-18 12:48:02 -0600

    Added a most popular color method, a total cars method and a cars per color method

--- cars.rb
+++ cars.rb
@@ -1,11 +1,34 @@
+
 class Car
+	attr_accessor :color 
+	@@total_car_count = 0
+	@@car_per_color = {}
+	#puts the total instances of the class Car
+  #it is a class method
+  def self.total_cars
+  	puts @@total_car_count
+  end
+  def self.most_popular_color
+  	@@car_per_color.sort_by {|k,v| v}
+  	@@car_per_color.keys[0]
+  end
+  def self.cars_per_color
+  	puts @@car_per_color
+  end
+	
 	def to_s()
 		"I'm a car! I've driven #{@distance} and have #{@fuel} gallons gas left"
 	end
 
-	def initialize()
+	def initialize(color)
 		@fuel = 10
 		@distance = 0
+		@@total_car_count += 1
+		if @@car_per_color[color] == nil
+			 @@car_per_color[color] = 1
+		else 
+			@@car_per_color[color] += 1
+		end
 	end
 
 	def drive(miles)
@@ -24,10 +47,14 @@ class Car
 		puts "You must pay $#{3.5 * gallons_needed}"
 		@fuel = 10.0
 	end
+ 
 end
 
-car_a = Car.new()
-car_b = Car.new()
+
+
+car_a = Car.new("blue")
+car_b = Car.new("red")
+car_c = Car.new("blue")
 puts car_a
 puts car_b
 car_a.drive(10)
@@ -37,3 +64,11 @@ car_a.drive(232)
 car_b.drive(117)
 puts car_a
 puts car_b
+Car.total_cars
+Car.most_popular_color
+best_color = Car.most_popular_color
+Car.new(best_color)
+Car.total_cars
+puts Car.cars_per_color
+
+
